63d98eca5822e6fe9b1344ec2390b42f6113a09d
Jean-Philippe Bidegain <jp@bidega.in>
2021-10-11 21:28:17 +0200

    fix: Create some vim directories used by vim

--- install.sh
+++ install.sh
@@ -1,12 +1,38 @@
 #!/bin/sh
 
 # Tmux plugin manager
-# Installs and loads tmux plugins.
-if [ -d ~/.tmux/plugins/tpm ] ; then
+# https://github.com/tmux-plugins
+if ! [ -d ~/.tmux/plugins/tpm ] ; then
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
 # Starship
-if ! command -v starship &> /dev/null
+# https://starship.rs
+if ! command -v starship &> /dev/null ; then
     sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+fi
+
+
+# Vim
+if ! [ -d ~/.vim/backups ] ; then
+    mkdir -p ~/.vim/backups
+fi
+
+if ! [ -d ~/.vim/swaps ] ; then
+    mkdir -p ~/.vim/swaps
+fi
+
+if ! [ -d ~/.vim/undo ] ; then
+    mkdir -p ~/.vim/undo
+fi
+
+if ! [ -d ~/.vim/plugged ] ; then
+    mkdir -p ~/.vim/plugged
+fi
+
+# Vim plug
+# https://github.com/junegunn/vim-plug
+if ! [ -f ~/.vim/autoload/plug.vim ] ; then
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
