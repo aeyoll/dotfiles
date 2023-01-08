b592fb89dddadf6a47f1847ffae5ffeecf06c3ee
Jean-Philippe Bidegain <jp@bidega.in>
2023-01-08 12:13:51 +0100

    refactor: move telescope configuration to a separate file

--- neovim/lua/configs.lua
+++ neovim/lua/configs.lua
@@ -51,13 +51,6 @@ require('nvim-treesitter.configs').setup {
   }
 }
 
--- Telescope
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
 -- Mason
 require("mason").setup()
 require("mason-null-ls").setup({
