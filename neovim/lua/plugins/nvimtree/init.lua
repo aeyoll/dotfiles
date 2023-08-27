bcb5723da3305394f18fcb61e4dcbb5c3daa5ee4
Jean-Philippe Bidegain <jp@bidega.in>
2023-08-27 10:48:12 +0200

    refactor: remove deprecated nvim configs

--- neovim/lua/plugins/nvimtree/init.lua
+++ neovim/lua/plugins/nvimtree/init.lua
@@ -7,12 +7,6 @@ require('nvim-tree').setup {
   },
   view = {
     adaptive_size = false,
-    mappings = {
-      list = {
-        { key = "u", action = "dir_up" },
-        { key = "g", action = "cd" },
-      },
-    },
   },
 }
 
